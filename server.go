4fcb9140efe13da2a38fd539ae873cdca0aa975b
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2013-09-04 16:24:00 +0900

    Refactored the server creation part

--- server.go
+++ server.go
@@ -34,19 +34,18 @@ func (pr *PageRequest)  New(path string, filename string, method string) {
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     path := r.URL.Path
+		log.Println(path)
     if path == "/" {
       path = "/index.html"
     }
     filename := "public"+path
     splitPath :=  strings.Split(path,"/")
-    fmt.Println(splitPath[len(splitPath)-1])
     if jsFileValidator.MatchString(splitPath[len(splitPath)-1]) {
       w.Header().Set("Content-Type", "text/javascript")
-      fmt.Println("It's a JS file")
     } else if cssFileValidator.MatchString(splitPath[len(splitPath)-1]) {
       w.Header().Set("Content-Type", "text/css")
-      fmt.Println("It's a CSS file")
-    }
+    } else {
+		}
     body, err := ioutil.ReadFile(filename)
     if err != nil {
       
@@ -70,7 +69,6 @@ func errorHandler(w http.ResponseWriter, r *http.Request, err error) {
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
   return func(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("Make Handler: %s\n", r.URL.Path)
     fn(w, r)
   }
 }
